f9996718c6141f9963f595d4dfaea919b6463a11
丘小天 <jonyau@qq.com>
2026-02-22 13:10:53 +0800

    rename: change brand name to Mishell

--- commit.bat
+++ commit.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "D:\CODING\丘小天的炒股生活\my-stock-site"
-git commit -m "feat: add miyou software page"
-git push
-pause
+git add .
+git commit -m "rename: change brand name to Mishell"
+git pull origin main --rebase
+git push origin main
