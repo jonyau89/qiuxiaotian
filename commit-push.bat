4a344b969491ff044e20e9543479e23dee27e159
丘小天 <jonyau@qq.com>
2026-02-26 17:12:06 +0800

    feat: add notify.json to lxd3 directory

--- commit-push.bat
+++ commit-push.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "D:\CODING\丘小天的炒股生活\my-stock-site"
 git add .
-git commit -m "feat: update download link to quark cloud"
+git commit -m "feat: add notify.json to lxd3 directory"
 git pull origin main --rebase
 git push origin main
 pause
